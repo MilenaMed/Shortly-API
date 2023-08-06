5ca3576ac8da35397cc6080d905b005e9597d11d
MilenaMed <115955806+MilenaMed@users.noreply.github.com>
2023-08-06 13:32:56 -0300

    POST - Sign up

--- src/app.js
+++ src/app.js
@@ -1,26 +1,48 @@
 import express from "express";
-import dotenv from "dotenv";
-import pg from "pg"
+import joi from "joi";
+import { db } from "./database/database.conection.js";
+import bcrypt from "bcrypt"
 
 const app = express();
 
 //Ferramentas
 app.use(express.json())
-dotenv.config()
 
-//SQL
-const { Pool } = pg
+//joi
+const signUpSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+    confirmPassword: joi.string().required()
+});
 
-const configDatabase = {
-  connectionString: process.env.DATABASE_URL,
-};
+//POST - signup
+app.post('/signup', async (request, response) => {
+    const { name, password, confirmPassword, email } = request.body;
+    const encryptedPassword = bcrypt.hashSync(password, 10);
+    const existingUser = await db.query(`SELECT * FROM users WHERE email=$1;`, [email])
 
-if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
+    const validation = signUpSchema.validate(request.body)
+    if (validation.error) {
+        return response.status(422).send("Preencha os dados corretamente")
+    }
 
-export const db = new Pool(configDatabase);
+    if (existingUser.rowCount !== 0) {
+        return response.status(409).send("Email já cadastrado")
+    }
+    if (password !== confirmPassword) {
+        return response.status(422).send("Senhas diferentes")
+    }
+
+    try {
+        await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`, [name, email, encryptedPassword])
+        return response.status(201).send("Usuário Cadastrado!")
+    } catch (err) {
+        return response.status(500).send(err.message)
+    }
+});
+
 
 //Porta
-const port = process.env.PORT || 5000
-app.listen(port, () => {
-	console.log(`Servidor rodando na porta ${port}`)
-})
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Running server on port ${PORT}`))
